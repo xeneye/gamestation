260f8cd7da503375c128a27a39681829b35f4ef6
Xeneye <paul@xeneye.com>
2026-07-24 12:12:54 +0100

    feat: add Roboto & Cascadia fonts

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,5 +4,5 @@ cp -avf "/ctx/system_files"/. /
 dnf5 install -y dnf-plugins-core
 dnf5 config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
 dnf5 install -y brave-origin
-dnf5 install -y google-roboto-condensed-fonts google-roboto-fonts google-roboto-mono-fonts google-roboto-slab-fonts cascadia-fonts-all
+dnf5 install -y google-roboto-fonts google-roboto-mono-fonts nerd-fonts cascadia-fonts-all
 systemctl enable podman.socket
