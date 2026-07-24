58ffd6c6009558bb9c3ed3ff261391039d53ffe6
Xeneye <paul@xeneye.com>
2026-07-24 09:48:52 +0100

    feat: add Brave Origin

--- build_files/build.sh
+++ build_files/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ouex pipefail
 cp -avf "/ctx/system_files"/. /
-dnf install dnf-plugins-core
-dnf config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
-dnf install brave-origin
+dnf5 install dnf-plugins-core
+dnf5 config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+dnf5 install brave-origin
 systemctl enable podman.socket
